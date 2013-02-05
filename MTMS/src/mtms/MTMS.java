8493b5dc5dcabf5e944d9bd5f7d5f8569611f191
Hui Guo <ecuguo@gmail.com>
2013-02-04 20:30:02 -0500

    Feasibility program

---
+++ MTMS/src/mtms/MTMS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mtms;
+
+/**
+ *
+ * @author user
+ */
+public class MTMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainJFrame().setVisible(true);
+            }
+        });
+    }
+}
